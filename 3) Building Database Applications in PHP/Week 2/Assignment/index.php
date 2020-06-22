071da8fd2e40984dd0ca082a4922f0317bc67669
Samarth Srivastava <63175337+ActuallySam@users.noreply.github.com>
2020-06-22 23:57:14 +0530

    Initials

---
+++ 3) Building Database Applications in PHP/Week 2/Assignment/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Samarth Srivastava</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<h1>Welcome to the Automobiles Database</h1>
+<a href = "login.php">Please Log In</a>
+<p>
+Attempt to go to
+<a href="view.php">view.php</a> without logging in - it should fail with an error message
+</p>
+<p>
+Attempt to go to
+<a href="add.php">add.php</a> without logging in - it should fail with an error message
+</p>
+</body>
+</html>
