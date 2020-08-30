f220460a007715e211746e6a7ab44cc2156f6dbb
Samarth Srivastava <63175337+ActuallySam@users.noreply.github.com>
2020-08-30 21:15:18 +0530

    Week1 materials added

---
+++ 4) JavaScript, JSON and JQuery/Week 1/Assignment/pdo.php
@@ -0,0 +1,11 @@
+<?php
+
+try{
+    $pdo = new PDO('mysql:host=127.0.0.1;port=3306;dbname=disc', 'fred', 'zap');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
